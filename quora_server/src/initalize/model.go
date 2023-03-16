477afa109eceb4fc5fad44e17181f8ce98ae074d
何鸿凯 <1544832671@qq.com>
2023-03-16 17:17:02 +0800

    feat: questionAPI

--- quora_server/src/initalize/model.go
+++ quora_server/src/initalize/model.go
@@ -3,10 +3,12 @@ package initalize
 import (
 	"gorm.io/gorm"
 	"quora_server/src/api/article"
+	"quora_server/src/api/question"
 	"quora_server/src/api/user"
 )
 
 func Model(DB *gorm.DB) {
 	DB.AutoMigrate(&user.User{})
 	DB.AutoMigrate(&article.Article{})
+	DB.AutoMigrate(&question.Question{})
 }
--- quora_server/src/main.go
+++ quora_server/src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"quora_server/src/api/article"
+	"quora_server/src/api/question"
 	"quora_server/src/middlewares"
 
 	"quora_server/src/api/user"
@@ -27,5 +28,12 @@ func main() {
 	r.PATCH("/v1/article/:articleId", user.LoginRequest, article.UpdateArticle)
 	r.GET("/v1/article/:articleId", article.GetArticle)
 	r.GET("/v1/user/articles", user.LoginRequest, article.GetUserArticles)
+
+	// question
+	r.POST("/v1/question", user.LoginRequest, question.UploadQuestion)
+	//r.DELETE("/v1/question/:questionId", user.LoginRequest, question.DeleteQuestion)
+	//r.PATCH("/v1/question/:questionId", user.LoginRequest, question.UpdateQuestion)
+	r.GET("/v1/question/:questionId", question.GetQuestion)
+	r.GET("/v1/user/questions", user.LoginRequest, question.GetUserQuestions)
 	r.Run(":9000") // 监听并在 0.0.0.0:8080 上启动服务
 }
